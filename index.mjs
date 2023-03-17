fdd057ee66618c91e2c2081e81690000033c2ac7
Maria <marfayaga@gmail.com>
2023-03-17 20:04:24 +0200

    add root route

--- index.mjs
+++ index.mjs
@@ -2,6 +2,10 @@ import fastify from "fastify";
 
 const server = fastify();
 
+server.get('/', (request, reply) => {
+  reply.send('Hello!')
+})
+
 server.listen({ port: 5432 })
   .then(() => {
     console.log('Successfully started')
